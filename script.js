1e6e10611699c1f4ec982a0c0aafb9b4b36064ba
Serayna Koch <e263213@sbl.ch>
2024-10-23 12:00:32 +0000

    Aufgabe 4 angefangen

--- script.js
+++ script.js
@@ -74,3 +74,20 @@ export function aufgabe03(args) {
   return count
 }
 linkupExerciseHandler("[data-click=aufgabe03]", aufgabe03)
+export function aufgabe04(args) {
+  const input = args
+  const result = []
+
+  let count = 0
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+
+    if (currentElement === "einfachtxur") {
+      count = count + 1
+    } else if (currentElement === "E") {
+      count++
+    }
+  }
+  return count
+}
+linkupExerciseHandler("[data-click=aufgabe04]", aufgabe04)
